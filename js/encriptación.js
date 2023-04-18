0988ced315f2a4ada013653dbb78a41e00e0083a
EnzoMartinez01 <enzomartinez2412@gmail.com>
2023-04-17 19:51:54 -0500

    primercommit

---
+++ js/encriptación.js
@@ -0,0 +1,31 @@
+function encriptar (){
+    var texto = document.querySelector("#input-texto").value;
+    var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+    document.querySelector(".text-input-salida").value = textoCifrado;
+    document.querySelector("#input-texto").value;
+    }
+
+
+    
+var boton1 = document.querySelector("#btn-encriptar"); boton1.onclick = encriptar;
+
+function desencriptar (){
+    var texto = document.querySelector("#input-texto").value;
+    var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+    document.querySelector(".text-input-salida").value = textoCifrado;
+    document.querySelector("#input-texto").value;
+
+}
+
+var boton2 = document.querySelector("#btn-desencriptar");
+boton2.onclick = desencriptar;
+
+function copiartexto(copy){
+        var range = document.createRange();
+        range.selectNode(copy);
+        window.getSelection().removeAllRanges(); 
+        window.getSelection().addRange(range); 
+        document.execCommand("copy");
+        window.getSelection().removeAllRanges();
+        console.log("Texto Copiado");
+}
